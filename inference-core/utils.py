119a938b7700604bb40411e595a000274223acaf
Lughaidar <19520504@gm.uit.edu.vn>
2022-11-23 17:03:54 +0700

    urgh

--- inference-core/utils.py
+++ inference-core/utils.py
@@ -1,13 +1,11 @@
+from typing import Union, Tuple
 from attrdict import AttrDict
 
-def echo_model_info(metadata: AttrDict, config: AttrDict):
-    print(f"Model: {metadata.name}. Available versions: {metadata.versions}")
-    print(f"Total input/output count: {len(metadata.inputs)} input(s) | {len(metadata.outputs)} output(s)")
-    
-    for count, inputs in enumerate(metadata.inputs):
-        print(f'> Input #{count}: {inputs.name}, shape: {inputs.shape}, datatype: {inputs.datatype}')  # type: ignore
-    for count, outputs in enumerate(metadata.outputs):
-        print(f'> Output #{count}: {outputs.name}, shape: {outputs.shape}, datatype: {outputs.datatype}') # type: ignore
-    
-    print(f"Max batch size: {config.max_batch_size}")
-    print(f"Input format: {config.input[0].format}") # type: ignore
+import json
+import glob
+import random
+
+import numpy as np
+import cv2
+
+from attrdict import AttrDict
